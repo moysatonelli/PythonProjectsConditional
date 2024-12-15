2cef04aa87ff079478f1e37444594028d306a9aa
moysatonelli <moysato.br@gmail.com>
2024-12-15 11:56:45 -0300

    change in code order

--- sales_control.py
+++ sales_control.py
@@ -6,7 +6,7 @@ banana = int(input("Digite a quantidade de bananas vendidas: "))
 
 if maca > banana:
     print(f"Maça foi a mais vendida: {maca}(UN)")
-elif maca == banana:
-    print(f"Houve um empate! Maçãs: {maca}(UN) | Bananas: {banana}(UN)")
 elif maca < banana:
     print(f"Banana foi a mais vendida: {banana}(UN)")
+elif maca == banana:
+    print(f"Houve um empate nas vendas! Maçãs: {maca}(UN) | Bananas: {banana}(UN)")
