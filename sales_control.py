2252f61a4a8057f51c66f82a8ee8c6e54773e60e
moysatonelli <moysato.br@gmail.com>
2024-12-15 11:53:43 -0300

    exercise description

--- sales_control.py
+++ sales_control.py
@@ -1,3 +1,6 @@
+#Crie um programa que receba o número de vendas dos dois produtos e exiba uma mensagem indicando qual deles vendeu mais.
+#Se as quantidades forem iguais, exiba uma mensagem dizendo que houve empate.
+
 maca = int(input("Digite a quantidade de maçãs vendidas: "))
 banana = int(input("Digite a quantidade de bananas vendidas: "))
 
